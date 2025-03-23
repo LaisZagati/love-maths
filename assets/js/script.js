764abe2ca811fefe6c8d84884c3a727b9a080fe5
LaisZagati <laisviana97@gmail.com>
2025-03-23 17:47:09 +0000

    Event listener: keydown enter and focus funcitons added

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,15 @@ document.addEventListener("DOMContentLoaded", function() {
                 runGame(gameType);
             }  
         })
-    }   
+    } 
+    
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
+
     runGame("addition");
 
 })
@@ -24,6 +32,10 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
@@ -127,7 +139,7 @@ function displaySubtractQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "-";
 
 }
-    
+
 function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
